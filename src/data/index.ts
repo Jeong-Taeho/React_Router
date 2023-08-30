f7ebc9bd2e8c251cb10c95daff7bd96bf88c92d4
Jeong-Taeho <nabi218218@gmail.com>
2023-08-30 13:52:07 +0900

    feat :  더미 데이터 생성

---
+++ src/data/index.ts
@@ -0,0 +1,22 @@
+export const pages = [
+  {
+    title: "Home",
+    screen: "Home Screen",
+    content: "Go to Details",
+  },
+  {
+    title: "Details1",
+    screen: "Details1 Screen",
+    content: "Go to Details2... ",
+  },
+  {
+    title: "Details2",
+    screen: "Details2 Screen",
+    content: "Go to Details3...",
+  },
+  {
+    title: "Details3",
+    screen: "Details3 Screen",
+    content: "Go to Details3... again",
+  },
+];
